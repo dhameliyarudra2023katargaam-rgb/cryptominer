15ca4ef5f87a3f926c607c694b1f8761105204e1
Rudra <dhameliyarudra2023katargaam@gmail.com>
2026-06-23 12:15:27 +0530

    notification functionality done and video bug solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
 android {
 //    namespace = "com.example.cryptominer"
     namespace = "com.app.projectminer1"
@@ -16,6 +17,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -43,6 +45,10 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
